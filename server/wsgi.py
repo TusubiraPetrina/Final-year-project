1660a437eaa4c8a9cb5764586bac080f2e3af953
George Owen Kivumbi <gokivumbi@Georges-MacBook-Pro.local>
2021-12-14 12:54:07 +0300

    edited procfile

--- server/wsgi.py
+++ server/wsgi.py
@@ -5,3 +5,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+""" if __name__ == '__main__':
+    application.run() """
